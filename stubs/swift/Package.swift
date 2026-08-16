b25cccc7aca98581608445f1130b5083b498c8b1
Bahram Shamshiri <jowharshamshiri@gmail.com>
2026-08-16 14:35:42 +0200

    Update capdag-objc dependency to v1.421.38

--- stubs/swift/Package.swift
+++ stubs/swift/Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .macOS(.v13)
     ],
     dependencies: [
-        .package(url: "https://github.com/machinefabric/capdag-objc.git", from: "1.420.33"),
+        .package(url: "https://github.com/machinefabric/capdag-objc.git", from: "1.421.38"),
         .package(url: "https://github.com/jowharshamshiri/ops-objc.git", from: "1.18.132"),
     ],
     targets: [
