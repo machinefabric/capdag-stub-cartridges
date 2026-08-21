fcb0d152123ab885ab1532207713accfd1897fef
Bahram Shamshiri <jowharshamshiri@gmail.com>
2026-08-21 16:22:21 +0200

    tests fix

--- stubs/swift/Package.swift
+++ stubs/swift/Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .macOS(.v13)
     ],
     dependencies: [
-        .package(url: "https://github.com/machinefabric/capdag-objc.git", from: "1.429.33"),
+        .package(url: "https://github.com/machinefabric/capdag-objc.git", from: "1.430.65"),
         .package(url: "https://github.com/jowharshamshiri/ops-objc.git", from: "1.19.17"),
     ],
     targets: [
