9d36703f39b505641e9fe09fb6e51d285b5c5d5e
Bahram Shamshiri <jowharshamshiri@gmail.com>
2026-08-23 17:09:20 +0200

    Update capdag-go dependency to version v1.374.70

--- stubs/go/cartridge.go
+++ stubs/go/cartridge.go
@@ -173,7 +173,7 @@ func buildManifest() *bifaci.CapManifest {
 
 	return capdag.NewCapManifest(
 		"__CARTRIDGE_NAME__",
-		"1.56.352",
+		"1.57.365",
 		"nightly", // 'nightly' or 'release'; nightly for dev.
 		nil,       // nil => dev cartridge (installed locally).
 		"Classify a piece of text as positive, neutral, or negative.",
