9e5d778b66cfdf4ba79f8c8bc58ed99b5eef3883
Bahram Shamshiri <jowharshamshiri@gmail.com>
2026-08-24 15:15:46 +0200

    machfab refactor

--- stubs/go/cartridge.go
+++ stubs/go/cartridge.go
@@ -173,7 +173,7 @@ func buildManifest() *bifaci.CapManifest {
 
 	return capdag.NewCapManifest(
 		"__CARTRIDGE_NAME__",
-		"1.66.470",
+		"1.67.483",
 		"nightly", // 'nightly' or 'release'; nightly for dev.
 		nil,       // nil => dev cartridge (installed locally).
 		"Classify a piece of text as positive, neutral, or negative.",
