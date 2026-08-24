0c1dbb09f109727d467a8bf30d1eabfa12345b65
Bahram Shamshiri <jowharshamshiri@gmail.com>
2026-08-24 10:21:32 +0200

    Update capdag dependency to version v1.696.0

--- stubs/swift/Sources/main.swift
+++ stubs/swift/Sources/main.swift
@@ -147,7 +147,7 @@ func buildManifest() -> Manifest {
 
     return Manifest(
         name: "__CARTRIDGE_NAME__",
-        version: "1.59.388",
+        version: "1.60.400",
         channel: "nightly",   // 'nightly' or 'release'; nightly for dev.
         registryURL: nil,     // nil => dev cartridge (installed locally).
         description: "Classify a piece of text as positive, neutral, or negative.",
