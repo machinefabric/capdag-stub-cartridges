9a61d7a925c55bbe8718ebbbf82d0e2529db9d58
Bahram Shamshiri <jowharshamshiri@gmail.com>
2026-08-24 17:32:42 +0200

    machfab proto refactor

--- stubs/python/cartridge.py
+++ stubs/python/cartridge.py
@@ -156,7 +156,7 @@ def build_manifest() -> CapManifest:
 
     return CapManifest(
         name="__CARTRIDGE_NAME__",
-        version="1.67.483",
+        version="1.68.496",
         channel="nightly",          # 'nightly' or 'release'; nightly for dev.
         registry_url=None,           # None => dev cartridge (installed locally).
         description="Classify a piece of text as positive, neutral, or negative.",
