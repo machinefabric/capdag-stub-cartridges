1a0546b4e139fab8a318d3f8fe2d4168eeb0bbd7
Bahram Shamshiri <jowharshamshiri@gmail.com>
2026-08-27 15:58:06 +0200

    cartridge daemon refactor

--- stubs/swift/Sources/main.swift
+++ stubs/swift/Sources/main.swift
@@ -147,7 +147,7 @@ func buildManifest() -> Manifest {
 
     return Manifest(
         name: "__CARTRIDGE_NAME__",
-        version: "1.75.587",
+        version: "1.76.600",
         channel: "nightly",   // 'nightly' or 'release'; nightly for dev.
         registryURL: nil,     // nil => dev cartridge (installed locally).
         description: "Classify a piece of text as positive, neutral, or negative.",
