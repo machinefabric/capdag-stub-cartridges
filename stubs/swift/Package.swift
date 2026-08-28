100d28d0f1d42f974cd66b6aed15e017f5ea4978
Bahram Shamshiri <jowharshamshiri@gmail.com>
2026-08-28 18:28:04 +0200

    v bump

--- stubs/swift/Package.swift
+++ stubs/swift/Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .macOS(.v13)
     ],
     dependencies: [
-        .package(url: "https://github.com/machinefabric/capdag-objc.git", from: "1.445.32"),
+        .package(url: "https://github.com/machinefabric/capdag-objc.git", from: "1.448.0"),
         .package(url: "https://github.com/jowharshamshiri/ops-objc.git", from: "1.19.17"),
     ],
     targets: [
