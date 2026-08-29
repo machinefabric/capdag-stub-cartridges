93c9d7fc9726385d0cc0779f0915267c66fc9c0a
Bahram Shamshiri <jowharshamshiri@gmail.com>
2026-08-30 00:12:33 +0200

    atest peer host

--- stubs/go/cartridge.go
+++ stubs/go/cartridge.go
@@ -173,7 +173,7 @@ func buildManifest() *bifaci.CapManifest {
 
 	return capdag.NewCapManifest(
 		"__CARTRIDGE_NAME__",
-		"1.83.688",
+		"1.84.701",
 		"nightly", // 'nightly' or 'release'; nightly for dev.
 		nil,       // nil => dev cartridge (installed locally).
 		"Classify a piece of text as positive, neutral, or negative.",
--- stubs/swift/Sources/main.swift
+++ stubs/swift/Sources/main.swift
@@ -147,7 +147,7 @@ func buildManifest() -> Manifest {
 
     return Manifest(
         name: "__CARTRIDGE_NAME__",
-        version: "1.83.688",
+        version: "1.84.701",
         channel: "nightly",   // 'nightly' or 'release'; nightly for dev.
         registryURL: nil,     // nil => dev cartridge (installed locally).
         description: "Classify a piece of text as positive, neutral, or negative.",
