a1adaf8cb83e129b22654cb33c7a1f2fff5e76b4
Bahram Shamshiri <jowharshamshiri@gmail.com>
2026-09-02 16:29:06 +0200

    ffmpeg fix for windows and others

--- stubs/swift/Package.swift
+++ stubs/swift/Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .macOS(.v13)
     ],
     dependencies: [
-        .package(url: "https://github.com/machinefabric/capdag-objc.git", from: "1.469.50"),
+        .package(url: "https://github.com/machinefabric/capdag-objc.git", from: "1.470.60"),
         .package(url: "https://github.com/jowharshamshiri/ops-objc.git", from: "1.19.17"),
     ],
     targets: [
