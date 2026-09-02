ee81be7be1445f465e35ddacc26148241fe73fb2
Bahram Shamshiri <jowharshamshiri@gmail.com>
2026-09-02 19:57:34 +0200

    atest explicit sessions

--- stubs/swift/Package.swift
+++ stubs/swift/Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .macOS(.v13)
     ],
     dependencies: [
-        .package(url: "https://github.com/machinefabric/capdag-objc.git", from: "1.471.62"),
+        .package(url: "https://github.com/machinefabric/capdag-objc.git", from: "1.472.64"),
         .package(url: "https://github.com/jowharshamshiri/ops-objc.git", from: "1.19.17"),
     ],
     targets: [
