fc696d7822903dbe15c9996091559ddbb4fdcf82
Bahram Shamshiri <jowharshamshiri@gmail.com>
2026-09-03 09:45:33 +0200

    atest explicit sessions

--- stubs/swift/Package.swift
+++ stubs/swift/Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .macOS(.v13)
     ],
     dependencies: [
-        .package(url: "https://github.com/machinefabric/capdag-objc.git", from: "1.473.66"),
+        .package(url: "https://github.com/machinefabric/capdag-objc.git", from: "1.474.68"),
         .package(url: "https://github.com/jowharshamshiri/ops-objc.git", from: "1.19.17"),
     ],
     targets: [
--- stubs/swift/Sources/main.swift
+++ stubs/swift/Sources/main.swift
@@ -147,7 +147,7 @@ func buildManifest() -> Manifest {
 
     return Manifest(
         name: "__CARTRIDGE_NAME__",
-        version: "1.103.944",
+        version: "1.104.957",
         channel: "nightly",   // 'nightly' or 'release'; nightly for dev.
         registryURL: nil,     // nil => dev cartridge (installed locally).
         description: "Classify a piece of text as positive, neutral, or negative.",
