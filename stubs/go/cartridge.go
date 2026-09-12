833350b5eaef1b47443e89dabedd3b77c455818f
Bahram Shamshiri <jowharshamshiri@gmail.com>
2026-09-13 00:16:52 +0200

    pve tests.

--- stubs/go/cartridge.go
+++ stubs/go/cartridge.go
@@ -173,7 +173,7 @@ func buildManifest() *bifaci.CapManifest {
 
 	return capdag.NewCapManifest(
 		"__CARTRIDGE_NAME__",
-		"1.142.1420",
+		"1.143.1431",
 		"nightly", // 'nightly' or 'release'; nightly for dev.
 		nil,       // nil => dev cartridge (installed locally).
 		"Classify a piece of text as positive, neutral, or negative.",
