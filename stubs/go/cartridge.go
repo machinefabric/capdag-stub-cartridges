5135ffec4914476acde78bd45507f39ef4184025
Bahram Shamshiri <jowharshamshiri@gmail.com>
2026-09-17 14:53:13 +0200

    sdx status knows local mode.

--- stubs/go/cartridge.go
+++ stubs/go/cartridge.go
@@ -173,7 +173,7 @@ func buildManifest() *bifaci.CapManifest {
 
 	return capdag.NewCapManifest(
 		"__CARTRIDGE_NAME__",
-		"1.155.1563",
+		"1.156.1574",
 		"nightly", // 'nightly' or 'release'; nightly for dev.
 		nil,       // nil => dev cartridge (installed locally).
 		"Classify a piece of text as positive, neutral, or negative.",
